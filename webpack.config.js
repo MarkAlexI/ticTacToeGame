825d2dd31a3983917302ee3ba57aa76286d1679c
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-10-24 19:23:32 +0300

    Add dependencies. Create basic structure.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env = {}) => ({
+  mode: env.prod ? "production" : "development",
+  entry: [
+    path.resolve(__dirname, "./src/main.js")
+  ].filter(Boolean),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        sideEffects: true,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              warnRuleAsWarning: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  stats: {
+    errorDetails: true,
+  },
+});
