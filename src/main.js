fb0ba527ace9d75c67e3843497309515e548fefd
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-11-06 04:15:56 +0200

    Add displaying turns of gamers.

--- src/main.js
+++ src/main.js
@@ -5,8 +5,16 @@ import { X_WON, O_WON, TIE, winConditions, gameBoard } from '@/constants';
 import { isValidMove, playerAction } from '@/actions';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const player = document.getElementById('player');
+  const status = document.getElementById('status');
   
-  eventBus.subscribe('move', message => console.log(`The player ${message} has made a move.`));
+  eventBus.subscribe('move', message => {
+    console.log(`The player ${message} has made a move.`);
+    status.classList.toggle('next');
+    player.classList.remove('X', 'O');
+    player.classList.add(message);
+    player.innerText = message;
+  });
   
   const cells = Array.from(document.querySelectorAll('.cell'));
   
